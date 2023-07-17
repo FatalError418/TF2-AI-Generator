a1f800ce6438b35ef5b86233428d3525adfa5198
FatalError418 <139549531+FatalError418@users.noreply.github.com>
2023-07-17 13:57:02 +1200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -3,5 +3,5 @@ python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pyinstaller --onefile --name app --icon=tf2_icon.ico --distpath compile --workpath compile gui.py weapongenerator.py
+pyinstaller --onefile --name app --add-data "tf2_icon.ico;." --icon=tf2_icon.ico --distpath compile --noconsole --workpath compile gui.py weapongenerator.py
 move app.spec compile
